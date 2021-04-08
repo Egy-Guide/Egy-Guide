c2b709523c22541ba08b548a3077afa4c5224b1e
Abdelsalam Abdrabou <abdulsalamabdrabo@gmail.com>
2021-04-08 14:50:00 +0200

    MoveBlogFunctionsToSD

--- EgyGuide.Utility/SD.cs
+++ EgyGuide.Utility/SD.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
+using System.Linq;
 
 namespace EgyGuide.Utility
 {
@@ -8,5 +10,19 @@ namespace EgyGuide.Utility
         public const string Role_User_Tourist = "Tourist";
         public const string Role_User_Tour_Guide = "Tour Guide";
         public const string Role_User_Suspended_Guide = "Suspended Guide";
-    }
+
+        public static string StripHtml(string htmlDescription)
+        {
+            var mainDoc = new HtmlDocument();
+            mainDoc.LoadHtml(htmlDescription);
+
+            return mainDoc.DocumentNode.InnerText;
+        }
+
+        public static string SubstringByWords(string text, int wordsNumber)
+        {
+            string newString = string.Join(" ", text.Split(" ").Take(wordsNumber));
+            return newString.TrimEnd('.');
+        }
+    }    
 }
